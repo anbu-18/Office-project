d0028c46cdca30c4119afcbaeb35d1d0ff664888
anbu-18 <84490218+anbu-18@users.noreply.github.com>
2021-11-29 12:17:18 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,30 +1,76 @@
 from flask import Flask, request, jsonify
 import malwaredetect
+import json
 
 app = Flask(__name__)
 
+########################## Main Page #########################################################
 @app.route('/')
 def index():
-    return "<h1>Webpage</h1>"
+    try:
+        return "<h1>Webpage</h1>"
+    except:
+        return "unable to load home page"
 
+########################## To insert URL into database from json request######################
 @app.route('/insertdata/',methods=["POST"])
 def insertdata():
-    data = request.json
-    obj = malwaredetect.malwaredetect()
-    response = obj.table_values(data)
-    return response
+    try:
+        data = request.json
+        obj = malwaredetect.malwaredetect()
+        response = obj.table_values(data)
+        return response
+    except:
+        return "Error while processing json request"
 
+######################### Get all values from a database #####################################
 @app.route('/getalldata/',methods=["GET"])
 def getalldata():
-    #data = request.json
-    obj = malwaredetect.malwaredetect()
-    response = obj.getallvalues()
-    return response
+    try:
+        obj = malwaredetect.malwaredetect()
+        response = obj.getallvalues()
+        return response
+    except:
+        return "Error while processing json request"
 
+######################### To check whether a url is available in database ###################
 @app.route('/malwarecheck/',methods=["POST"])
 def malwarecheck():
-    data = request.json
-    obj = malwaredetect.malwaredetect()
-    response = obj.detection(data)
-    return response    
+    try:
+        data = request.json
+        obj = malwaredetect.malwaredetect()
+        response = obj.detection(data)
+        return response
+    except:
+        return "Error while processing json request"
+        
+
+######################### To insert the URL from a text file into database ###############
+@app.route('/getfile/',methods=["POST"])
+def getfile():
+    try:
+        file = request.files['file']
+        fileContent = file.read().decode("utf-8")
+        replaceWhitespace=fileContent.replace("\n", ",").strip()
+        splitter = replaceWhitespace.split(",")
+        for i in splitter:
+            if i.strip() != "":
+                userrequest = {'url':i.strip()}
+                obj=malwaredetect.malwaredetect()
+                response=obj.table_values(userrequest)
+        return response
+    except:
+        return "Error while processing input file"
+
+################################ To clear values from a database ##########################
+@app.route('/deleteall/',methods=["DELETE"])
+def deleteall():
+    try:
+        obj= malwaredetect.malwaredetect()
+        response = obj.deleteall()
+        return response
+    except:
+        return "Error while processing json request"
+        
+
         
